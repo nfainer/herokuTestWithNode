c7ccfc8ad22136c55e67a6e6ad009bb95540dc46
Your Name <n_fainer@fanshaweonline.ca>
2019-09-23 19:30:52 -0400

    added express

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const express = require('express');
-const port = 3000;
+const path = require('path');
+// const port = 3000;
+
+const port = process.env.PORT || 3000
 
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  console.log('at the home route')
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+})
+
+// a forward slash is the home route (same as index.html)
+app.get('/contact', (req, res) => {
+  console.log('at the contact route')
+  res.sendFile(path.join(__dirname + '/views/contact.html'));
+})
+
+app.get('/portfolio', (req, res) => {
+  console.log
+  res.send('on the portfolio page!');
 })
 
 
-app.get('/contact'. (req, res) => {
-  res.send('on the contact page!');
-})
 
-
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running at ${port}/`);
 });
